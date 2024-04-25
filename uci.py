d83e15679a4b06c8da8677443ff19e4698d82a8b
Jakub Krzywoń <91898433+JakubKrz@users.noreply.github.com>
2024-04-25 16:41:40 +0200

    split

--- uci.py
+++ uci.py
@@ -1,4 +1,9 @@
 from ucimlrepo import fetch_ucirepo
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 # fetch dataset 
 phiusiil_phishing_url_website = fetch_ucirepo(id=967)
 
@@ -11,3 +16,10 @@ print(phiusiil_phishing_url_website.metadata)
   
 # variable information 
 print(phiusiil_phishing_url_website.variables) 
+
+
+# Split dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+classifier = DecisionTreeClassifier()
+
