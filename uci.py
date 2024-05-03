3228f956e0b0eb2529b99b48b61833cb35839efb
Jakub Krzywoń <91898433+JakubKrz@users.noreply.github.com>
2024-05-03 13:08:10 +0200

    to do

--- uci.py
+++ uci.py
@@ -12,19 +12,29 @@ X = phiusiil_phishing_url_website.data.features
 y = phiusiil_phishing_url_website.data.targets
 
 # metadata 
-print(phiusiil_phishing_url_website.metadata) 
-  
+print(phiusiil_phishing_url_website.metadata)   
 # variable information 
 print(phiusiil_phishing_url_website.variables) 
-
 
 # Split dataset into training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-classifier = DecisionTreeClassifier()
+#TO DO: Change categorical columns into encoded(numeric values) columns
+#1 - one-hot encoding 2 - label encoding 3- ordinal encoding
+
 #finding categorical columns
 categorical_indexes = []
 for idx, column in enumerate(phiusiil_phishing_url_website.variables.columns):
     if phiusiil_phishing_url_website.variables.loc[idx, 'type'] == 'Categorical':
         categorical_indexes.append(idx)
 
+# Create Decision Tree classifer object
+DTC = DecisionTreeClassifier()
+# Train Decision Tree Classifer
+DTC = DTC.fit(X_train,y_train)
+#Predict the response for test dataset
+y_pred = DTC.predict(X_test)
+#Model accuracy
+metrics.accuracy_score(y_test, y_pred)
+
+#TO DO: add visualization of decision tree (use graphviz and pydotplus?)
